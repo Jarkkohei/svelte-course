5cea1d9274aadae52cee0e864fbdb5e50eca342d
Jarkko <jarkkohei@luukku.com>
2019-05-17 10:08:15 +0300

    Remove redundant validity variable definitions

--- src/Meetups/EditMeetup.svelte
+++ src/Meetups/EditMeetup.svelte
@@ -8,24 +8,11 @@
     const dispatch = createEventDispatcher();
 
     let title = '';
-    let titleValid = false;
-
     let subtitle = '';
-    let subtitleValid = false;
-
     let address = '';
-    let addressValid = false;
-
     let imageUrl = '';
-    let imageUrlValid = false;
-
     let email = '';
-    let emailValid = false;
-
     let description = '';
-    let descriptionValid = false;
-
-    let formIsValid = false;
 
     $: titleValid = !isEmpty(title);
     $: subtitleValid = !isEmpty(subtitle);
