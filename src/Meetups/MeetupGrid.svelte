f477b12de31d71f6b4d5a207a1682a47d148c85a
Jarkko <jarkkohei@luukku.com>
2019-05-21 01:45:46 +0300

    Add MeetupFilter -component and implement filtering the meetups by favorite status

--- src/Meetups/MeetupGrid.svelte
+++ src/Meetups/MeetupGrid.svelte
@@ -1,26 +1,43 @@
 <script>
     import MeetupItem from './MeetupItem.svelte';
+    import MeetupFilter from './MeetupFilter.svelte';
 
     export let meetups;
+
+    let favsOnly = false;
+
+    $: filteredMeetups = favsOnly ? meetups.filter(m => m.isFavorite) : meetups;
+
+    function setFilter(event) {
+        favsOnly = event.detail === 1;
+    }
 </script>
 
 <style>
-    section {
+    #meetups {
         width: 100%;
         display: grid;
         grid-template-columns: 1fr;
         /*grid-gap: 1rem;*/
-     }
+    }
 
-     @media (min-width: 768px) {
-         section {
-             grid-template-columns: repeat(2, 1fr);
-         }
-      }
+    #meetup-controls {
+        margin: 1rem;
+    }
+
+    @media (min-width: 768px) {
+        #meetups {
+            grid-template-columns: repeat(2, 1fr);
+        }
+    }
 </style>
 
-<section>
-    {#each meetups as meetup}
+<section id="meetup-controls">
+    <MeetupFilter on:select={setFilter} />
+</section>
+
+<section id="meetups">
+    {#each filteredMeetups as meetup}
         <MeetupItem {...meetup} on:showdetails on:edit />
     {/each}
 </section>
