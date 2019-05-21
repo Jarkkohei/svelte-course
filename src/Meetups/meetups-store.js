8aebb428cc5dfaf28dc279ef0257682580e7130f
Jarkko <jarkkohei@luukku.com>
2019-05-22 02:28:10 +0300

    Remove the dummy meetups data

--- src/Meetups/meetups-store.js
+++ src/Meetups/meetups-store.js
@@ -1,27 +1,6 @@
 import { writable } from 'svelte/store';
 
-const meetups = writable([
-    {
-        id: 'm1',
-        title: 'Coding bootcamp',
-        subtitle: 'Learn to code in 2 hours',
-        description: 'In this meetup we have experts to teach us how to code.',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/6/6b/Meetup_Logo.png',
-        address: '27th Nerd Road, 432543 New York',
-        contactEmail: 'code@test.com',
-        isFavorite: false,
-    },
-    {
-        id: 'm2',
-        title: 'Coding bootcamp 2',
-        subtitle: 'Learn to code in 4 hours',
-        description: 'In this meetup we have experts to teach us how to code some more.',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/6/6b/Meetup_Logo.png',
-        address: '27th Nerd Road, 432543 New York',
-        contactEmail: 'code@test.com',
-        isFavorite: false,
-    }
-]);
+const meetups = writable([]);
 
 const customMeetupsStore = {
     subscribe: meetups.subscribe,
